cb419c26f83236d44e7c9c85b341efe4e97f4906
Andrey Klimchuk <aklimchu@e1r6p6.unit.ua>
2018-10-06 23:40:59 +0300

    ...

--- hyper/src/hyper_http.erl
+++ hyper/src/hyper_http.erl
@@ -18,7 +18,11 @@ init(#{path := <<"/api/", Path/binary>>, method := Method} = Req0, State) ->
     Req1 = set_cors_headers(Req0),
     Req2 = case Method of
                <<"GET">> -> get(Path, Req1);
-               <<"POST">> -> post(Path, Req1);
+               <<"POST">> ->
+                   case validate_post(Path, ?BODY(Req)) of
+                       {ok, Body} -> post(Path, Body, Req1);
+                       E -> E
+                   end,
                <<"OPTIONS">> -> cowboy_req:reply(200, Req1);
                _ -> cowboy_req:reply(405, Req1)
            end,
@@ -29,42 +33,49 @@ terminate(_Reason, _Req, _State) ->
 
 get(<<"auth">>, Req) -> ok;
 get(<<"auth/google/", Action/binary>>, Req) ->
-    social_auth(<<"google">>, Action, Req);
+    reply(social_auth(<<"google">>, Action, Req), Req);
 get(<<"auth/github/", Action/binary>>, Req) ->
-    social_auth(<<"github">>, Action, Req);
+    reply(social_auth(<<"github">>, Action, Req), Req);
 get(<<"auth/registration/confirm">>, Req) ->
-    auth_reply(hyper_auth:register(cowboy_req:parse_qs(Req)), Req);
+    reply(hyper_auth:register(cowboy_req:parse_qs(Req)), Req);
 get(<<"auth/lostpass/confirm">>, Req) ->
-    auth_reply(hyper_auth:recover_password(cowboy_req:parse_qs(Req)), Req);
+    reply(hyper_auth:recover_password(cowboy_req:parse_qs(Req)), Req);
 get(<<"profile/email/confirm">>, Req) ->
     reply(hyper_user:update_email(cowboy_req:parse_qs(Req)), Req);
-get(Path, Req) -> protected_action(Path, Req, get/3).
+get(Path, Req) ->
+    case hyper:parse_token(Req) of
+        {ok, UData} -> get(Path, Req, UData);
+        error -> cowboy_req:reply(404, Req)
+    end.
 
 get(<<"user/", Uname>>, Req, UData) ->
-    reply(hyper_user:update_email(cowboy_req:parse_qs(Req)), Req);
+    reply(hyper:get_user(cowboy_req:parse_qs(Req)), Req);
 get(<<"profile">>, Req, UData) ->
-    reply(hyper_user:update_email(cowboy_req:parse_qs(Req)), Req);
+    reply(hyper:get_profile(cowboy_req:parse_qs(Req)), Req);
 get(_, Req0, _) -> cowboy_req:reply(404, Req0).
 
 
-post(<<"auth/logout">>, Req) ->
-    auth_reply(hyper_auth:logout(cowboy_req:parse_cookies(Req)), Req);
-post(<<"auth/login">>, Req) ->
-    auth_reply(hyper_auth:login(password, ?BODY(Req)), Req);
-post(<<"auth/registration">>, Req) ->
-    auth_reply(hyper_auth:register(?BODY(Req), ?BASE_URL(Req)), Req);
-post(<<"auth/lostpass">>, Req) ->
-    auth_reply(hyper_auth:recover_password(?BODY(Req), ?BASE_URL(Req)), Req);
-post(<<"auth/lostpass/newpass">>, Req) ->
-    auth_reply(hyper_auth:set_new_password(?BODY(Req), cowboy_req:parse_cookies(Req)), Req);
-post(Path, Req) -> protected_action(Path, Req, post/3).
+post(<<"auth/logout">>, Body, Req) ->
+    reply(hyper_auth:logout(cowboy_req:parse_cookies(Req)), Req);
+post(<<"auth/login">>, Body, Req) ->
+    reply(hyper_auth:login(password, ?BODY(Req)), Req);
+post(<<"auth/registration">>, Body, Req) ->
+    reply(hyper_auth:register(?BODY(Req), ?BASE_URL(Req)), Req);
+post(<<"auth/lostpass">>, Body, Req) ->
+    reply(hyper_auth:recover_password(?BODY(Req), ?BASE_URL(Req)), Req);
+post(<<"auth/lostpass/newpass">>, Body, Req) ->
+    reply(hyper_auth:set_new_password(?BODY(Req), cowboy_req:parse_cookies(Req)), Req);
+post(Path, Body, Req) -> protected_action(Path, Body, Req, post/4).
 
-
-post(<<"profile">>, Req, UData) -> reply(with_validation(update_profile, Req, UData), Req);
-post(<<"profile/email">>, Req, UData) -> reply(with_validation(update_email, Req, UData), Req);
-post(<<"profile/pass">>, Req, UData) -> reply(with_validation(update_pass, Req, UData), Req);
-post(<<"comment">>, Req, UData) -> reply(with_validation(live_comment, Req, UData), Req);
-post(_, Req, _) -> cowboy_req:reply(404, Req).
+post(<<"profile">>, Body, Req, UData) ->
+    reply(hyper:update_profile(Body, UData), Req);
+post(<<"profile/email">>, Body, Req, UData) ->
+    reply(hyper:update_email(Body, UData), Req);
+post(<<"profile/pass">>, Body, Req, UData) ->
+    reply(hyper:update_pass(Body, UData), Req);
+post(<<"comment">>, Body, Req, UData) ->
+    reply(hyper:live_comment(Body, UData), Req);
+post(_, _, Req, _) -> cowboy_req:reply(404, Req).
 
 
 
@@ -79,49 +90,36 @@ read_body(Req0, Acc) ->
 
 social_auth(Network, Action, #{qs := Qs} = Req) ->
     case hyper_oauth2:dispatch(?BASE_URL(Req), Network, Action, Qs) of
-        {redirect, Uri} -> cowboy_req:reply(302, #{<<"location">> => Uri}, Req);
-        {send_html, Html} -> cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, Html, Req);
         {profile, Profile} ->
             lager:info("Profile: ~p", [Profile]),
-            auth_reply(hyper_auth:login(social, Profile), Req);
-        {error, E} ->
-            lager:error("Error: ~p", [E]),
-            cowboy_req:reply(500, Req)
+            hyper_auth:login(social, Profile);
+        E -> E
     end.
 
-protected_action(Path, Req, Fun) ->
+protected_action(Path, Body, Req, Fun) ->
     case hyper:parse_token(Req) of
-        {ok, UData} -> Fun(Path, Req, UData);
+        {ok, UData} -> Fun(Path, Body, Req, UData);
         error -> cowboy_req:reply(404, Req)
     end.
 
-with_validation(FunName, Req) ->
-    case liver:validate(hyper_validation:get_schema(FunName), ?BODY(Req)) of
-        {ok, Payload} -> hyper:FunName(Payload);
-        E -> E
-    end.
 
-with_validation(FunName, Req, UData) ->
-    case liver:validate(hyper_validation:get_schema(FunName), ?BODY(Req)) of
-        {ok, Payload} -> hyper:FunName(Payload, UData);
-        E -> E
-    end.
-
-reply(ok, Req) -> ?REPLY(200, #{status => ok}, Req);
-reply({ok, Data}, Req) ->  ?REPLY(200, #{status => ok, data => Data}, Req);
-reply({error, Reason}, Req) -> ?REPLY(200, #{status => error, reason => Reason}, Req);
+reply(ok, Req) ->
+    ?REPLY(200, #{status => ok}, Req);
+reply({ok, Data = #{}}, Req) ->
+    ?REPLY(200, #{status => ok, payload => Data}, Req);
+reply({ok, Mod}, Req) when is_tuple(Mod) ->
+    reply(ok, mod_req(Mod, Req));
+reply({ok, Data = #{}, Mod}, Req) when is_tuple(Mod) ->
+    reply({ok, Data}, mod_req(Mod, Req));
+reply({error, Reason}, Req) ->
+    ?REPLY(200, #{status => error, reason => Reason}, Req);
 reply({redirect, Uri}, Req) ->
     TUri = <<(?FRONTEND_ORIGIN)/binary, Uri/binary>>,
-    cowboy_req:reply(302, #{<<"location">> => TUri}, Req).
-
-auth_reply(ok, Req) -> reply(ok, Req);
-auth_reply({ok, Mod}, Req) -> reply(ok, mod_req(Mod, Req));
-auth_reply({ok, Data, Mod}, Req) -> reply({ok, Data}, mod_req(Mod, Req));
-auth_reply({redirect, _} = R, Req) -> reply(R, Req);
-auth_reply({redirect, Uri, Mod}, Req) -> reply({redirect, Uri}, mod_req(Mod, Req));
-auth_reply({error, Reason} = E, Req) ->
-    lager:error("Error: ~p", [Reason]),
-    reply(E, Req).
+    cowboy_req:reply(302, #{<<"location">> => TUri}, Req);
+reply({redirect, Uri, Mod}, Req) when is_tuple(Mod) ->
+    reply({redirect, Uri}, mod_req(Mod, Req));
+reply({send_html, Html}, Req) ->
+    cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, Html, Req).
 
 mod_req({delete_cookie, Name}, Req) -> cowboy_req:set_resp_cookie(Name, <<>>, Req, #{path => "/", max_age => 0});
 mod_req({set_cookie, Name, Value}, Req) -> cowboy_req:set_resp_cookie(Name, Value, Req, #{path => "/"}).
