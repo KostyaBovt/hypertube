f9df5acf7e658c709f83783341fd9c0ab61a15cb
Roman TARASENKO <rtarasen@e1r8p5.unit.ua>
2018-10-15 20:12:48 +0300

    removed all react-materialize code

--- frontend-react/src/components/Login.js
+++ frontend-react/src/components/Login.js
@@ -1,7 +1,4 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { Row, Input, Button, Col } from "react-materialize";
 import "../../node_modules/materialize-social/materialize-social.css";
 import intra_logo from "../img/42.png";
 
@@ -10,31 +7,6 @@ class Login extends Component {
 		console.log('rendering login');
 		return (
 			<div className="container-log">
-				<Row>
-				 	<Input type="email" label="Email or User Name" s={12} m={12} l={12} xl={12}/>
-				    <Input type="password" label="Password" s={12} m={12} l={12} xl={12}/>
-				    	<button className="btn waves-effect waves-light btn-small" type="submit" name="action">Submit
-    					<i className="material-icons right">send</i>
-  					</button>
-  					<div className="social">
-
-  					<Col s={3} m={3} l={3} xl={3} >
-						<a className="waves-effect waves-red btn-floating social google">
-							<i className="fab fa-google"></i> Sign in with google
-						</a>
-						</Col>
-						<Col s={3} m={3} l={3} xl={3} >
-						<a className="waves-effect waves-light btn-floating ">
-						<img src={intra_logo} alt="42_intra" width="40" height="40"/>
-						</a>
-						</Col>
-						<Col s={3} m={3} l={3} xl={3} >
-						<a className="waves-effect waves-light btn-floating social github">
-							<i className="fab fa-github"></i> Sign in with github
-						</a>
-						</Col>
-					</div>
-				</Row>	
 			</div>
 		);
 	}
--- frontend-react/src/components/LostPass.js
+++ frontend-react/src/components/LostPass.js
@@ -1,19 +1,9 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { Row, Input } from "react-materialize";
 
 class LostPass extends Component {
 	render() {
 		return (
-			<div className="container-log">
-			<Row>
-				<Input type="password" label="New Password" s={12} />
-				<Input type="password" label="Repeat Password" s={12} />
-				 <button className="btn waves-effect waves-light btn-small" type="submit" name="action">Submit
-    				<i className="material-icons right">send</i>
-  				</button>
-				</Row>		
+			<div className="container-log">	
 			</div>
 		);
 	}
