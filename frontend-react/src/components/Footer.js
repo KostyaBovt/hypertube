785f2cdff24a48f59c2a21ea43622d6b9b5e9fb8
Lyana MALAYA <lmalaya@e1r8p3.unit.ua>
2018-10-15 20:52:04 +0300

    footer

--- frontend-react/src/components/Footer.js
+++ frontend-react/src/components/Footer.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  footer: {
+    backgroundColor: '#4d4d4d',
+    color: '#f2f2f2',
+    textAlign: 'center',
+  },
+};
 
 class Footer extends Component {
 	render() {
+		const { classes } = this.props;
 		return (
-			<nav className="footer text-center navbar-light">
+			<Typography className={classes.footer}>
 	  			<div className="foter">aklimchu lmalaya kbovt rtarasen</div>
 	  			<div>© 2018</div> 
-			</nav>
+			 </Typography>
+
 		);
 	}
 };
 
-export default Footer;
+export default withStyles(styles)(Footer);
--- frontend-react/src/components/Login.js
+++ frontend-react/src/components/Login.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
 import "../../node_modules/materialize-social/materialize-social.css";
+import { inject, observer } from 'mobx-react';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import intra_logo from "../img/42.png";
 
+@inject('AuthStore') @observer
 class Login extends Component {
 	render() {
 		console.log('rendering login');
