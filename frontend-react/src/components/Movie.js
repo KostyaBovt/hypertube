36a2960c54efa129e79c2250a0e72c888fb3b00a
Roman TARASENKO <rtarasen@student.unit.ua>
2018-11-18 21:21:47 +0200

    movie component improved

--- frontend-react/src/components/Movie.js
+++ frontend-react/src/components/Movie.js
@@ -1,23 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import { Typography, Card, ButtonBase, IconButton, CircularProgress, List, ListSubheader, ListItemText, ListItem} from '@material-ui/core';
-import not_found from "../img/not_found.jpg";
+import { Typography, CircularProgress } from '@material-ui/core';
 import ReactPlayer from 'react-player';
 
 
 const styles = theme => ({
-	// layout: {
-	// 	width: 'auto',
-	// 	[theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
-	// 		width: 600,
-	// 		marginLeft: 'auto',
-	// 		marginRight: 'auto',
-	// 	}
-    // },
     layout: {
         marginLeft: 'auto',
         marginRight: 'auto',
@@ -31,14 +22,18 @@ const styles = theme => ({
     item : {
         margin: theme.spacing.unit
     },
-    poster: {
+    media: {
         width: "100%",
     }
 });
 
-@inject('SelfStore') 
-@inject('MovieStore') @observer
+@inject('MovieStore', 'SelfStore') @observer
 class Movie extends Component {
+    constructor(props) {
+        super(props);
+        this.renderPlayer = this.renderPlayer.bind(this);
+    }
+
     componentDidMount() {
         this.props.MovieStore.fetchMovieDetails(this.props.match.params.id);
     }
@@ -46,30 +41,29 @@ class Movie extends Component {
     componentWillUnmount() {
         this.props.MovieStore.resetMovie();
     }
+
     renderPlayer(){
         console.log(this.props.SelfStore.self.locale);
-        const { stream } = this.props.MovieStore;
-        if (stream) {
-            console.log(stream.subs);
-            return (<ReactPlayer
-                        controls
-                        crossOrigin="anonymous"
-                        url = {stream.movie_link}
-                        config = {{ file: {
-                            attributes: {
-                                crossOrigin: 'true'
-                            },
-                            tracks: Object.entries(stream.subs).map(a => {
-                                return {kind: 'subtitles',
-                                        src: a[1],
-                                        srcLang: a[0],
-                                        default: a[0] == this.props.SelfStore.self.locale}
-                            })   
-                        }}}
-                     />)
-        } else {
-            return null;
-        }
+        const { MovieStore, classes } = this.props;
+        const { movie } = MovieStore;
+
+        return (
+            <ReactPlayer
+                controls={true}
+                
+                width="100%"
+                height="100%"
+                url={["http://localhost:3200/film/tt5463162/1080", "http://localhost:3200/film/tt5463162/720"]}
+                config={{
+                    file: {
+                        attributes: {
+                            poster: "http://image.tmdb.org/t/p/w500/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg"
+                        },
+                        tracks: []
+                    }
+                }}
+            />
+        )
     }
 
     render() {
@@ -83,14 +77,14 @@ class Movie extends Component {
         } else {
             return (
                 <main className={classes.layout}>
-                    <Grid container className={classes.container} justify="center">
+                    <Grid container spacing={16} className={classes.container} direction="column" justify="center" alignItems="center">
 
-                        <Grid item xs={6}>
-                            <Paper square>
+                        <Grid item xs={12} md={6}>
+                            <Paper>
                                 <Grid container>
 
                                     <Grid item xs={5} className={classes.item}>
-                                        <img className={classes.poster} src={movie.poster_path} alt="Movie poster"/>
+                                        <img className={classes.media} src={movie.poster_path} alt="Movie poster"/>
                                     </Grid>
 
                                     <Grid item xs className={classes.item}>
@@ -118,10 +112,21 @@ class Movie extends Component {
                                             
                                         </Grid>
                                     </Grid>
+
                                 </Grid>
                             </Paper>
                         </Grid>
-                        {this.renderPlayer()}
+
+                        <Grid item xs={12} md={6}>
+                            <Grid container>
+                                <Paper>
+                                    <Grid item className={classes.item}>
+                                        { this.renderPlayer() }
+                                    </Grid>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+
                     </Grid>
                 </main>
             );
