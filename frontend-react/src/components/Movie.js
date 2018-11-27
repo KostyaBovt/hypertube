332d1ed48b3c9b8ba14cf4cc5ade733f8070ec2d
Roman TARASENKO <rtarasen@student.unit.ua>
2018-11-27 20:32:42 +0200

    a little visual improvement for Movie page

--- frontend-react/src/components/Movie.js
+++ frontend-react/src/components/Movie.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import { TextField, Typography, CircularProgress, FilledInput, InputLabel, Icon, Button, FormControl} from '@material-ui/core';
+import { TextField, Typography, CircularProgress, Button, Avatar} from '@material-ui/core';
 import ReactPlayer from 'react-player';
 import Plyr from 'react-plyr';
 
@@ -15,15 +14,13 @@ const styles = theme => ({
         marginTop: theme.spacing.unit * 2,
 		marginBottom: theme.spacing.unit * 2,
     },
-    container: {
+    root: {
         marginTop: theme.spacing.unit * 2,
-        marginBottom: theme.spacing.unit * 2
+        marginBottom: theme.spacing.unit * 2,
+        marginLeft: 'auto',
+        marginRight: 'auto',
     },
-    container2: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    item : {
+    item: {
         margin: theme.spacing.unit
     },
     playerContainer: {
@@ -33,27 +30,17 @@ const styles = theme => ({
     media: {
         width: "100%",
     },
-    iconSmall: {
-        fontSize: 10,
+    reactPlayer: {
+        padding: 0,
+        margin: 0
     },
-    button: {
-        // margin: theme.spacing.unit,
-        // width: 100,
-        // marginLeft: 445,
-        // marginTop: 15,
-        // padding: 10
+    paper: {
+        padding: theme.spacing.unit * 2
     },
-    formControl: {
-        width: "100%",
-        // marginLeft: 25,
-  },
-  fo: {
-    wordBreak: "break-all"
-  },
-  reactPlayer: {
-      padding: 0,
-      margin: 0
-  }
+    avatar: {
+		margin: theme.spacing.unit,
+		backgroundColor: theme.palette.grey,
+	}
 });
 
 @inject('MovieStore', 'SelfStore') @observer
@@ -66,12 +53,15 @@ class Movie extends Component {
         };
         this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.resetCommentInput = this.resetCommentInput.bind(this);
     }
 
     componentDidMount() {
         const { movieId } = this.state;
-        this.props.MovieStore.fetchMovieDetails(movieId);
-        this.props.MovieStore.fetchComments(movieId);
+        const { MovieStore } = this.props;
+
+        MovieStore.fetchMovieDetails(movieId);
+        MovieStore.fetchComments(movieId);
     }
 
     componentWillUnmount() {
@@ -83,40 +73,51 @@ class Movie extends Component {
             commentValue: e.target.value
         });
     }
+
+    resetCommentInput() {
+        this.setState({ commentValue: '' });
+    }
+
     handleSubmit(e) {
         e.preventDefault();
-        this.props.MovieStore.postComment(this.state.movieId, this.state.commentValue);
-        this.setState({
-            commentValue: ''
-        });
+
+        const { MovieStore } = this.props;
+        const { movieId, commentValue } = this.state;
+
+        MovieStore.postComment(movieId, commentValue);
+        this.resetCommentInput();
     }
 
     renderPlayer(){
-        console.log(this.props.SelfStore.self.locale);
         const { MovieStore, classes } = this.props;
         const { movie } = MovieStore;
 
         return (
-            <Grid item xs={12} md={6}>
+            <Grid item>
                 <Paper square className={classes.playerContainer}>
                     <ReactPlayer
                         controls
                         className={classes.reactPlayer}
-                        url={`http://localhost:3200/film/${movie.imdb_id}/720`}
-                        // config={{
-                        //     file: {
-                        //         attributes: {
-                        //             crossOrigin: 'true'
-                        //         },
-                        //         tracks: [
-                        //             {
-                        //                 kind: 'subtitles',
-                        //                 src: `http://localhost:3200/subtitles/${movie.imdb_id}/en`,
-                        //                 srcLang: 'en'
-                        //             }
-                        //         ]
-                        //     }
-                        // }}
+                        url={movie.streaming}
+                        config={{
+                            file: {
+                                attributes: {
+                                    crossOrigin: "use-credentials"
+                                },
+                                tracks: [
+                                    {
+                                        kind: 'subtitles',
+                                        src: `http://localhost:3200/subtitles/${movie.imdb_id}/720p/en`,
+                                        srcLang: 'en'
+                                    },
+                                    {
+                                        kind: 'subtitles',
+                                        src: `http://localhost:3200/subtitles/${movie.imdb_id}/720p/ru`,
+                                        srcLang: 'ru'
+                                    }
+                                ]
+                            }
+                        }}
                         width='100%'
                         height='100%'
                     />
@@ -125,8 +126,46 @@ class Movie extends Component {
         )
     }
 
+    renderAvatar(self, classes) {
+		if (self.avatar) {
+			return <Avatar className={classes.avatar} src={`http://localhost:8080${self.avatar}`} />;
+		} else {
+			return (
+				<Avatar className={classes.avatar} src={self.avatar} >
+					{`${self.fname.charAt(0)}${self.lname.charAt(0)}`}
+				</Avatar>
+			);
+		}
+    }
+
+    renderCommentSectionActions() {
+        return (
+            <Grid container justify="flex-end">
+                <Grid item>
+                    <Button
+                        onClick={this.resetCommentInput}
+                        variant="text"
+                    >
+                        Cancel
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button
+                        onClick={this.handleSubmit}
+                        variant="text"
+                        color="primary"
+                    >
+                        Send
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    }
+
     render() {
         const { classes } = this.props;
+        const { commentValue } = this.state;
+        const { self } = this.props.SelfStore; 
         const { movie, comments } = this.props.MovieStore;
 
         if (movie === undefined) {
@@ -135,104 +174,99 @@ class Movie extends Component {
             return "ERROR";
         } else {
             return (
-                <main className={classes.layout}>
+                <main>
 
-                    <Grid container spacing={16} className={classes.container} direction="column" justify="center" alignItems="center">
+                    <Grid item xs={12} md={6} className={classes.root}>
+                        <Grid container spacing={16} className={classes.container} direction="column">
 
-                        <Grid item xs={6} md={6}>
-                            <Paper>
-                                <Grid container>
-                                    <Grid item xs={4} className={classes.item}>
-                                        <img className={classes.media} src={movie.poster_path} alt="Movie poster"/>
-                                    </Grid>
-                                    <Grid item xs className={classes.item}>
-                                        <Grid container direction="column">
-                                            <Grid item>
-                                                <Typography variant="h6">
-                                                    {movie.title}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography gutterBottom variant="subtitle2" color="textSecondary">
-                                                    {movie.release_date}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography gutterBottom variant="subtitle2" color="textSecondary">
-                                                    {movie.runtime} minutes
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography variant="subtitle1">
-                                                    {movie.overview}
-                                                </Typography>
+                            <Grid item>
+                                <Paper>
+                                    <Grid container>
+
+                                        <Grid item xs={4} className={classes.item}>
+                                            <img className={classes.media} src={movie.poster_path} alt="Movie poster"/>
+                                        </Grid>
+                                        <Grid item xs className={classes.item}>
+                                            <Grid container direction="column">
+                                                <Grid item>
+                                                    <Typography variant="h6">
+                                                        {movie.title}
+                                                    </Typography>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Typography gutterBottom variant="subtitle2" color="textSecondary">
+                                                        {movie.release_date}
+                                                    </Typography>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Typography gutterBottom variant="subtitle2" color="textSecondary">
+                                                        {movie.runtime} minutes
+                                                    </Typography>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Typography variant="subtitle1">
+                                                        {movie.overview}
+                                                    </Typography>
+                                                </Grid>
                                             </Grid>
                                         </Grid>
+
                                     </Grid>
+                                </Paper>
+                            </Grid>
 
-                                </Grid>
-                            </Paper>
-                        </Grid>
+                            { !!movie.streaming && this.renderPlayer() }
 
-                        { this.renderPlayer() }
-
-                        <Grid item xs={6} md={6} >
-                            <Grid container>
-                                <Paper className={classes.formControl} >
-                                     <form className={classes.container2} noValidate autoComplete="off">
-                                        <TextField 
-                                          value={this.state.commentValue}
-                                          onChange={this.handleInput}
-                                          style={{ margin: 8 }}
-                                          placeholder="Comment"
-                                          fullWidth
-                                          margin="normal"
-                                          InputLabelProps={{
-                                            shrink: true,
-                                          }}
-                                        />    
-                                        <Button
-                                            onClick={this.handleSubmit}
-                                            variant="contained"
-                                            color="primary"
-                                            className={classes.button}>
-                                        Send
-                                        <Icon className={classes.rightIcon}>send</Icon>
-                                        </Button>
+                            <Grid item>
+                                <Paper className={classes.paper} >
+                                    <form noValidate autoComplete="off">
+                                        <Grid container alignItems="flex-start">
+                                            <Grid item>
+                                                {  this.renderAvatar(self, classes) }
+                                            </Grid>
+                                            <Grid item xs>
+                                                <TextField
+                                                    value={this.state.commentValue}
+                                                    onChange={this.handleInput}
+                                                    placeholder="Leave a comment"
+                                                    margin="dense"
+                                                    fullWidth
+                                                    multiline
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                        { commentValue && this.renderCommentSectionActions() }
                                     </form>
                                 </Paper>
                             </Grid>
-                        </Grid>
-                        
+                            
 
-                        <Grid item xs={6} md={6} >
-                            <Grid container>
-                                <Paper>
-                                    { comments.map( c => (
-                                        <Grid item className={classes.item}>
-                                            <p>{ c.uname }</p>
-                                            <Typography
-                                                variant="p"
-                                                component="p"
-                                                className={classes.fo}>
-                                                { c.text }
-                                            </Typography>
-                                            <p>{ c.dt }</p>
-                                        </Grid>
-                                    ))}
-                                </Paper>
+                            <Grid item>
+                                <Grid container>
+                                    <Paper>
+                                        { comments.map( c => (
+                                            <Grid item className={classes.item}>
+                                                <p>{ c.uname }</p>
+                                                <Typography
+                                                    variant="p"
+                                                    component="p"
+                                                    className={classes.fo}>
+                                                    { c.text }
+                                                </Typography>
+                                                <p>{ c.dt }</p>
+                                            </Grid>
+                                        ))}
+                                    </Paper>
+                                </Grid>
                             </Grid>
-                        </Grid>
 
+                        </Grid>
                     </Grid>
+
                 </main>
             );
         }
     }
 }
-
-Movie.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(Movie);
