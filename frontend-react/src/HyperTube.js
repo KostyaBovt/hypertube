49176042116f56bd0e7d86d619e37eff7f39ea49
Roman TARASENKO <rtarasen@student.unit.ua>
2018-12-01 16:51:55 +0200

    Library is a homepage now

--- frontend-react/src/HyperTube.js
+++ frontend-react/src/HyperTube.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './HyperTube.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './components/Home';
 import Movie from './components/Movie';
 import Auth from './components/Auth';
 import NotFound from './components/NotFound';
@@ -44,11 +43,10 @@ class HyperTube extends Component {
                           <Header/>
                           <Switch>
                               <Route path="/auth" component={Auth}/>
-                              <PrivateRoute exact path="/" component={Home}/>
+                              <PrivateRoute exact path="/" component={Library}/>
                               <PrivateRoute exact path="/user/:username" component={User}/>
                               <PrivateRoute exact path="/settings" component={SettingsPage}/>
                               <PrivateRoute exact path="/movie/:id([0-9]*)" component={Movie}/>
-                              <PrivateRoute exact path="/library" component={Library}/>
                               <Route path="*" component={NotFound}/>
                           </Switch>
                           <Footer/>
