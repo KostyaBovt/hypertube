7735615ce2c81d8424ee430b1fa7ce090e9110f5
Roman TARASENKO <rtarasen@student.unit.ua>
2018-12-05 19:25:27 +0200

    oops, fixed intra icon

--- frontend-react/src/components/Login.js
+++ frontend-react/src/components/Login.js
@@ -102,7 +102,7 @@ class Login extends Component {
 					</Button>
 
 					<div className="social">
-						<a title="42_Intra" className={classes.intra} href="http://localhost:8080/api/auth/intra/login">
+						<a title="42_Intra" className={classes.intraIco} href="http://localhost:8080/api/auth/intra/login">
 							<img src={intra_logo} height="20"/>
 						</a>
 						<a title="GitHub" href="http://localhost:8080/api/auth/github/login">
