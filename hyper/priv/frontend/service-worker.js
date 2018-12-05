3d6c20dd32c86dd5e6ca030fdb6126a343a14845
Andrey Klimchuk <aklimchu@e1r2p10.unit.ua>
2018-12-05 21:16:33 +0200

    updated TODO.txt and frontend files

--- hyper/priv/frontend/service-worker.js
+++ hyper/priv/frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1f8a9a20740d8b49e2804f330f7d058.js"
+  "/precache-manifest.6f6a2a74883966aad4a74748c83174c7.js"
 );
 
 workbox.clientsClaim();
